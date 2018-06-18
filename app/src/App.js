bfd238133c19e902fe7e10a43a26443f539ae3ff
Daskott <edmondcotterell@gmail.com>
2018-06-17 23:09:57 -0400

    updated schema & included relay env

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { graphql, QueryRenderer } from "react-relay";
+import logo from "./logo.svg";
+import "./App.css";
+import environment from "./Environment";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query AppBooksQuery {
+            searchBooks(filter: "t", first: 10) {
+              edges {
+                node {
+                  title
+                  author
+                }
+              }
+            }
+          }
+        `}
+        variables={{}}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+
+          console.log({ props });
+          return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+            </div>
+          );
+        }}
+      />
     );
   }
 }
